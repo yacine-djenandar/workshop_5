bc0c0a885d3957025fde5c51bac84838269f4b6c
Mohammed Yacine Djenandar <62435906+yacine-djenandar@users.noreply.github.com>
2026-01-04 02:02:40 +0100

    Update sentiment.py

--- src/models/sentiment.py
+++ src/models/sentiment.py
@@ -3,7 +3,8 @@ from textblob import TextBlob
 def extract_sentiment(text: str):
     '''Extract sentiment using textblob.
     Polarity is within range [-1, 1]'''
-
+    print("Yaaay I edited this file")
+    
     text = TextBlob(text)
 
     return text.sentiment.polarity
